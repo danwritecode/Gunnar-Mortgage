238fa7a01da8e9a8a64d04c18826b79cc1ead1ab
Dan Nelson <dnelson4993@outlook.com>
2020-10-12 18:22:46 -0400

    updated footer again with contact info

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,7 +18,22 @@
             </svg>
           </a>
         </div>
-        <div class="mt-8 md:mt-0 md:order-1 flex items-center justify-center">
+        <div class="mt-8 md:mt-0 md:order-1">
+          <div class="mt-4 flex justify-center sm:justify-start items-center text-white">
+            <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+            </svg>
+            <p class="font-medium ml-2">gunnar@gunnar.mortgage</p>
+          </div>
+          <div class="mt-1 flex justify-center sm:justify-start items-center text-white">
+            <svg class="h-5 w-5"  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+            </svg>
+            <p class="font-medium ml-2">(215) 870-8724</p>
+          </div>
+        </div>
+      </div>
+      <div class="flex items-center justify-center pb-4">
           <div class="mr-4">
             <p class="text-center text-base leading-6 text-white">
               NMLS Company 1734358
@@ -33,7 +48,6 @@
           <div>
             <img src="/house.png" alt="" class="w-20 h-auto">
           </div>
-        </div>
       </div>
     </footer>
 
