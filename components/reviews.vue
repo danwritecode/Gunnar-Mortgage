a1dd36153a239ab1b4794b9b56c653584f0fb8e0
nelsynelz <dnelson4993@outlook.com>
2021-02-16 21:39:29 -0500

    updated reviews, changed styles on headers, cleaned up footer, added gradient to icon boxes

--- components/reviews.vue
+++ components/reviews.vue
@@ -1,34 +1,16 @@
 <template>
-  <!-- <div class="relative max-w-xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-screen-xl py-24 lg:py-40">
-    <div class="mb-16">
-      <h3 id="learn-more" class="text-3xl sm:text-5xl lg:text-6xl leading-none font-extrabold text-gray-900 tracking-tight mb-1">
-        Reviews
-      </h3>
-      <p class="max-w-4xl text-lg sm:text-2xl font-medium sm:leading-10 space-y-6 mb-6 text-gray-600">
-        See what everyone is saying about Gunnar.
-      </p>
-    </div>
-    <ul class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
-      <li :key="review.time" v-for="review in reviews" class="col-span-1 flex flex-col text-center bg-white rounded-lg border border-gray-200 shadow">
-        <div class="flex-1 flex flex-col p-8">
-          <img class="w-32 h-32 flex-shrink-0 mx-auto bg-black rounded-full" :src="review.profile_photo_url" alt="">
-          <h3 class="mt-3 text-gray-900 text-base leading-5 font-medium">{{ capitalize(review.author_name) }}</h3>
-          <div class="mt-6">
-            <div class="inline-flex items-center">
-              <div class="flex-shrink-0 flex">
-                <svg :key="rating" v-for="rating in review.rating" class="h-5 w-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                </svg>
-              </div>
-            </div>
-          </div>
-          <p id="reviewText" class="text-left mt-3 text-base text-gray-700">{{ review.text }}</p>
-        </div>
-      </li>
-    </ul>
-  </div> -->
-
   <div class="pb-24 lg:pb-64 overflow-hidden">
+    <div class="mx-auto max-w-xl px-4 sm:px-6 lg:px-8 lg:max-w-screen-xl">
+      <div class="flex-shrink-0 mb-8">
+        <div class="flex items-center justify-center h-12 w-12 rounded-md bg-gradient-to-br from-gb-400 via-gb-500 to-indigo-500 text-white">
+          <svg class="h-8 w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
+          </svg>
+        </div>
+      </div>
+      <p class="sm:text-lg sm:leading-snug font-semibold tracking-wide uppercase text-gb-600 mb-3">TRANSPARENCY</p>
+    </div>
+
     <div class="mb-16 mx-auto max-w-xl px-4 sm:px-6 lg:px-8 lg:max-w-screen-xl">
       <h3 id="learn-more" class="text-3xl sm:text-5xl lg:text-6xl leading-none font-extrabold text-gray-900 tracking-tight mb-1">
         Reviews
@@ -37,9 +19,9 @@
         See what everyone is saying about Gunnar.
       </p>
     </div>
-    <div class="flex items-center overflow-hidden" style="width:1000vw;">
-      <div :key="review.time" v-for="review in reviews" class="mx-4 relative rounded-lg border border-gray-300" v-bind:style="styleObject">
-        <div class="p-8">
+    <div class="flex items-center overflow-hidden py-10" :style="styleObject">
+      <div :key="review.Id" v-for="review in reviews" class="mx-4 relative rounded-lg border border-gray-200 shadow-lg hover:shadow-xl" style="width:30rem;">
+        <div class="p-4 sm:p-8">
           <blockquote>
             <div>
               <svg class="h-10 w-10 text-gb-600 opacity-25" fill="currentColor" viewBox="0 0 32 32" aria-hidden="true">
@@ -60,7 +42,7 @@
             </div>
           </div>
         </div>
-        <div class="bottom-0 w-full bg-gradient-to-r from-gb-400 to-gb-600 py-4 px-6 rounded-b-lg flex items-center">
+        <div class="bottom-0 w-full py-3 px-5 sm:py-4 sm:px-6 rounded-b-lg flex items-center bg-gradient-to-br from-gb-400 via-gb-500 to-indigo-500">
           <div>
             <img class="border-2 border-white w-10 h-10 flex-shrink-0 mx-auto bg-black rounded-full" :src="review.profile_photo_url" alt="">
           </div>
@@ -75,38 +57,64 @@
 </template>
 
 <script>
+import { uuid } from 'vue-uuid'
+
 export default {
   data() {
     return {
       reviews: [],
-      curTranslateXValue: 10
+      curTranslateXValue: 15
     }
   },
   computed: {
     styleObject() {
-      setTimeout(() => this.curTranslateXValue += -0.1, 1)
+      setTimeout(() => this.curTranslateXValue += -0.15, 1)
       const randomDeg = Math.random()
       const randomPosNeg = randomDeg > 0.5 ? randomDeg * -1:randomDeg * 1;
 
       return {
-        transform: 'translateX(' + this.curTranslateXValue + 'px) rotate(' + randomPosNeg + 'deg)',
-        width: '30rem'
+        transform: 'translateX(' + this.curTranslateXValue + 'px)',
+        width: '10000vw'
       }
     }
   },
   mounted() {
     this.getReviews();
-    setTimeout(() => this.curTranslateXValue += -0.1, 1)
   },
   methods: {
     async getReviews() {
-      const reviewsResp = await this.$axios.$get('http://localhost:8888/.netlify/functions/fetchReviews')
-      this.reviews = reviewsResp.GoogleResponse
+      const reviewsResp = await this.$axios.$get('https://gunnar.mortgage/.netlify/functions/fetchReviews')
+      const reviewsArray = reviewsResp.GoogleResponse
+
+      for (var m = 0; m < 5; m++) {
+        for (var i = 0; i < reviewsArray.length; i++) {
+          var tempArray = JSON.parse(JSON.stringify(reviewsArray[i]));
+          const randomDeg = Math.random(0.75)
+          const randomPosNeg = randomDeg > 0.5 ? randomDeg * -1:randomDeg * 1;
+          tempArray.DegRotate = randomPosNeg
+          tempArray.Id = uuid.v1()
+          this.reviews.push(tempArray)
+        }
+      }
     },
     capitalize(str) {
       return str.replace(/\w\S*/g, function(txt){
           return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
       });
+    },
+    style_GetRandomFooterGradient() {
+      const styles = [
+        'bg-gradient-to-br from-purple-500 to-indigo-500',
+        'bg-gradient-to-br from-yellow-400 to-orange-500',
+        'bg-gradient-to-br from-pink-500 to-rose-500',
+        'bg-gradient-to-br from-cyan-400 to-light-blue-500',
+        'bg-gradient-to-br from-orange-400 to-pink-600',
+        'bg-gradient-to-br from-green-400 to-cyan-500',
+        'bg-gradient-to-br from-purple-500 to-indigo-500',
+        'bg-gradient-to-br from-yellow-400 to-orange-500'
+      ]
+
+      return styles[Math.floor(Math.random() * styles.length)];
     }
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
         <h3 class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl lg:text-5xl xl:text-6xl">Mortgages the <span class="text-gb-600">Gunnar</span> way</h3>
         <p class="mt-3 text-lg sm:text-2xl font-medium sm:leading-10 space-y-6 mb-6 text-gray-600">Make your mortgage experience painless with Gunnar Mortgage. The one-stop shop for purchasing and refinancing.</p>
         <div class="rounded-md shadow-lg mt-5 sm:mt-8">
-          <a href="https://gunnarmortgage.floify.com/" target="_blank" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-gb-600 hover:bg-gb-500 focus:outline-none focus:border-gb-700 focus:shadow-outline-gb transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
+          <a href="https://www.blink.mortgage/app/signup/p/GunnarMortgage" target="_blank" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-gb-600 hover:bg-gb-500 focus:outline-none focus:border-gb-700 focus:shadow-outline-gb transition duration-300 ease-in-out md:py-4 md:text-lg md:px-10">
             Get started
           </a>
         </div>
@@ -17,6 +17,30 @@
 
     <div class="pb-40 overflow-hidden lg:pb-64">
       <div class="relative max-w-xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-screen-xl">
+        
+        <div>
+          <div class="flex-shrink-0 mb-8">
+            <div class="flex items-center justify-center h-12 w-12 rounded-md bg-gradient-to-br from-gb-400 via-gb-500 to-indigo-500 text-white">
+              <svg class="h-8 w-8 fill-current mx-auto" viewBox="0 0 512.001 512.001">
+                <g>
+                  <g>
+                    <path d="M507.606,4.394c-3.339-3.339-8.055-4.909-12.728-4.243L346.387,21.364c-3.688,0.527-7.047,2.406-9.425,5.273
+                      L139.321,264.938c-16.353-7.263-36.394-4.357-50.074,9.323l-21.213,21.213c-5.854,5.854-5.857,15.356,0,21.213l31.82,31.82
+                      l-48.64,48.64H45c-24.813,0-45,20.187-45,45V467c0,24.813,20.187,45,45,45h24.853c24.813,0,45-20.187,45-45v-6.213l48.639-48.64
+                      l31.82,31.82c5.854,5.854,15.356,5.857,21.213,0l21.212-21.213c13.638-13.638,16.611-33.666,9.323-50.075l238.302-197.641
+                      c2.866-2.378,4.746-5.737,5.273-9.425L511.85,17.121C512.517,12.447,510.945,7.732,507.606,4.394z M84.853,467
+                      c0,8.271-6.729,15-15,15H45c-8.271,0-15-6.729-15-15v-24.853c0-8.271,6.729-15,15-15h6.213l33.64,33.64V467z M99.853,433.36
+                      L78.64,412.147l42.426-42.426l21.213,21.213L99.853,433.36z M216.525,401.54l-10.606,10.606L99.853,306.081l10.607-10.606
+                      c5.863-5.863,15.349-5.863,21.213,0l84.853,84.853C222.388,386.191,222.388,395.677,216.525,401.54z M461.757,155.642
+                      L228.075,349.451l-22.156-22.157l174.408-174.408c5.858-5.858,5.858-15.355,0-21.213c-5.857-5.858-15.355-5.858-21.213,0
+                      L184.706,306.081l-22.156-22.156L356.358,50.244l122.964-17.566L461.757,155.642z"/>
+                  </g>
+                </g>
+              </svg>
+            </div>
+          </div>
+          <p class="sm:text-lg sm:leading-snug font-semibold tracking-wide uppercase text-gb-600 mb-3">MODERN OPERATION</p>
+        </div>
 
         <div class="relative">
           <h3 id="learn-more" class="text-3xl sm:text-5xl lg:text-6xl leading-none font-extrabold text-gray-900 tracking-tight mb-1">
@@ -33,7 +57,7 @@
               <div class="col-span-4">
                 <div class="flex">
                   <div class="flex-shrink-0">
-                    <div class="flex items-center justify-center h-12 w-12 rounded-md bg-gb-600 text-white">
+                    <div class="flex items-center justify-center h-12 w-12 rounded-md bg-gradient-to-br from-gb-400 via-gb-500 to-indigo-500 text-white">
                       <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
                       </svg>
@@ -62,7 +86,7 @@
                   <div class="ml-4">
                     <h5 class="text-lg leading-6 font-medium text-gray-900">One Lender</h5>
                     <p class="mt-2 text-base leading-6 text-gray-600">
-                      With one lender you have the rate provided and that's it. That's your flexbility.
+                      With one lender you have the terms provided and that's it. That's your flexibility.
                     </p>
                   </div>
                 </div>
@@ -72,7 +96,7 @@
               <div class="col-span-4">
                 <div class="flex">
                   <div class="flex-shrink-0">
-                    <div class="flex items-center justify-center h-12 w-12 rounded-md bg-gb-600 text-white">
+                    <div class="flex items-center justify-center h-12 w-12 rounded-md bg-gradient-to-br from-gb-400 via-gb-500 to-indigo-500 text-white">
                       <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 11V7a4 4 0 118 0m-4 8v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2z" />
                       </svg>
@@ -81,7 +105,7 @@
                   <div class="ml-4">
                     <h5 class="text-lg leading-6 font-medium text-gray-900">Competitive Rates</h5>
                     <p class="mt-2 text-base leading-6 text-gray-600">
-                      Gunnar works for himself, by himself. Costs are low and rates are even lower.
+                      Gunnar works for himself, by himself. Fees are low and rates are even lower.
                     </p>
                   </div>
                 </div>
@@ -99,9 +123,9 @@
                     </div>
                   </div>
                   <div class="ml-4">
-                    <h5 class="text-lg leading-6 font-medium text-gray-900">No Rate Flexibilty</h5>
+                    <h5 class="text-lg leading-6 font-medium text-gray-900">No Rate FlexibilIty</h5>
                     <p class="mt-2 text-base leading-6 text-gray-600">
-                      With a bank you have one option, a higher rate due to the increased overhead. It's the opposite with Gunnar.
+                      With a bank you have one option, a higher rate and fees due to the increased overhead. It's the opposite with Gunnar.
                     </p>
                   </div>
                 </div>
@@ -111,7 +135,7 @@
               <div class="col-span-4">
                 <div class="flex">
                   <div class="flex-shrink-0">
-                    <div class="flex items-center justify-center h-12 w-12 rounded-md bg-gb-600 text-white">
+                    <div class="flex items-center justify-center h-12 w-12 rounded-md bg-gradient-to-br from-gb-400 via-gb-500 to-indigo-500 text-white">
                       <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
                       </svg>
@@ -155,7 +179,7 @@
       <reviews />
     </div>
 
-    <div class="relative bg-gray-800 border-b-4 border-white">
+    <div class="relative bg-gray-800">
       <div class="h-56 bg-gb-600 sm:h-72 md:absolute md:left-0 md:h-full md:w-1/2">
         <img class="w-full h-full object-cover" src="/cta_banner.png" alt="Support team">
       </div>
@@ -168,7 +192,7 @@
             Gunnar's here to help. Killer rates, better communication, the overall best experience.
           </p>
           <div class="rounded-md shadow mt-5 sm:mt-8">
-            <a href="https://gunnarmortgage.floify.com/" target="_blank" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-gb-600 hover:bg-gb-500 focus:outline-none focus:border-gb-700 focus:shadow-outline-gb transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
+            <a href="https://www.blink.mortgage/app/signup/p/GunnarMortgage" target="_blank" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-gb-600 hover:bg-gb-500 focus:outline-none focus:border-gb-700 focus:shadow-outline-gb transition duration-300 ease-in-out md:py-4 md:text-lg md:px-10">
               Get started
             </a>
           </div>
